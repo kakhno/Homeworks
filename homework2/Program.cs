9c06b906061adec99d583080a4ff47de60aa2e1a
tsotne <kakhniashvili1011@gmail.com>
2024-09-24 12:44:25 -0700

    homework2

---
+++ homework2/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Homework2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.BackgroundColor = ConsoleColor.Blue;
+
+            Console.WriteLine("Tsotne Kakhniashvili");
+
+            Console.Write("Input: ");
+            string input = Console.ReadLine();
+            Console.Write(input);
+
+        }
+    }
+}
