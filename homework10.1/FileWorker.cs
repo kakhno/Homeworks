570f627255b42d345f34fb9d269c25bfa4395484
tsotne <kakhniashvili1011@gmail.com>
2024-10-16 22:03:32 -0700

    Homework 10 Solution 1

---
+++ homework10.1/FileWorker.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace homework10._1
+{
+    internal abstract class FileWorker
+    {
+        public int FileMaxStorage { get; set; }
+
+        public string FileType { get; set; }
+
+        public abstract string GetFileType();
+
+        public FileWorker(int FileMaxStorage) 
+        {
+            this.FileMaxStorage = FileMaxStorage;
+        }
+
+        public void SetFileType(string type) 
+        {
+            FileType = type;
+        }
+
+        public virtual void Write()
+        {
+            Console.WriteLine($"I can write to {FileType} file with max storage {FileMaxStorage}");
+        }
+
+        public void Read()
+        {
+            Console.WriteLine($"I can read from {FileType} file with max storage {FileMaxStorage}");
+        }
+
+        public void Delete()
+        {
+            Console.WriteLine($"I can delete from {FileType} file with max storage {FileMaxStorage}");
+        }
+        
+        public void Edit()
+        {
+            Console.WriteLine($"I can edit {FileType} file with max storage {FileMaxStorage}");
+        }
+
+
+    }
+
+    internal class Nextclass : FileWorker
+    {
+        public Nextclass(int FileMaxStorage) : base(FileMaxStorage)
+        {
+            SetFileType(".txt");
+        }
+
+        public override string GetFileType()
+        {
+            return FileType;
+        }
+    }
+}
---
+++ homework10.1/Program.cs
@@ -0,0 +1,14 @@
+﻿using homework10._1;
+using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        var objct = new Nextclass(128);
+        objct.Write();
+        objct.Read();
+        objct.Delete();
+        objct.Edit();
+    }
+}
