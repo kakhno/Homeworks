2c9c4db00d706e7fb0bb280d1e9d1df321a4ef1a
tsotne <kakhniashvili1011@gmail.com>
2024-10-16 22:04:01 -0700

    Homework 10 Solution 2

---
+++ homework10.2/Finance.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace homework10._2
+{
+    interface IFinanceOperations
+    {
+        void CalculateLoanPercent(int month, double AmountPerMonth);
+        bool CheckUserHistory();
+
+    }
+
+    //  BANK CLASS ------------ 
+    internal class Bank : IFinanceOperations
+    {
+        public int TotalAmount { get; set; }
+        public double AmountPerMonth { get; set; }
+        public int Month { get; set; }
+
+        public void CalculateLoanPercent(int month, double AmountPerMonth)
+        {
+            Console.WriteLine((month * (TotalAmount * AmountPerMonth)) + TotalAmount);
+            // itvlis jamshi ramdenis gadaxda mouwevs
+        }
+
+        public bool CheckUserHistory()
+        {
+            Random random = new Random();
+            return random.Next(2) == 0;
+        }
+    }
+
+
+    // MICROFINANCE CLASS -----------------
+    internal class MicroFinance : IFinanceOperations
+    {
+        public int TotalAmount { get; set; }
+        public double AmountPerMonth { get; set; }
+        public int Month { get; set; }
+        public void CalculateLoanPercent(int month, double AmountPerMonth)
+        {
+            Console.WriteLine((month * (TotalAmount * AmountPerMonth)) + TotalAmount + month * 4);
+        }
+
+        public bool CheckUserHistory()
+        {
+            return true;
+        }
+    }
+}
---
+++ homework10.2/Program.cs
@@ -0,0 +1,54 @@
+﻿using homework10._2;
+using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        var bank = new Bank();
+        var microfinance = new MicroFinance();
+
+        Console.Write("Please enter which do you want -- Bank or MicroFinance: ");
+        string WhichSystem = Console.ReadLine();
+
+        if (WhichSystem.ToLower() == "bank")
+        {
+            if (!bank.CheckUserHistory())
+            {
+                Console.WriteLine("False User History");
+            }
+            else
+            {
+                Console.WriteLine("True User History");
+
+                Console.Write("Please enter amount of gel: ");
+                bank.TotalAmount = int.Parse(Console.ReadLine());
+
+                Console.Write("Please enter amount of month: ");
+                bank.Month = int.Parse(Console.ReadLine());
+
+                Console.Write("Please enter percentage(0.05 = 5%): ");
+                bank.AmountPerMonth = double.Parse(Console.ReadLine());
+
+                Console.Write("Total amount to pay: ");
+                bank.CalculateLoanPercent(bank.Month, bank.AmountPerMonth);
+            }
+        }
+            else if (WhichSystem.ToLower() == "microfinance")
+            {
+                Console.Write("Please enter amount of gel: ");
+                microfinance.TotalAmount = int.Parse(Console.ReadLine());
+
+                Console.Write("Please enter amount of month: ");
+                microfinance.Month = int.Parse(Console.ReadLine());
+
+                Console.Write("Please enter percentage(0.1 = 10%): ");
+                microfinance.AmountPerMonth = double.Parse(Console.ReadLine());
+
+                Console.Write("Total amount to pay: ");
+                microfinance.CalculateLoanPercent(microfinance.Month, microfinance.AmountPerMonth);
+            }
+
+        }        
+    }
+
